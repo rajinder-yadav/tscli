407cdc3db13118033f7b750e3d0b0ac661fa85d1
Rajinder Yadav <rajinder.yadav@hotmail.com>
2017-02-23 21:10:54 -0500

    Moved test folder to co-locate.

---
+++ src/lib/test/test.utils.ts
@@ -0,0 +1,12 @@
+import * as test from "tape";
+import * as utils from "../utils";
+
+test( "hello", t => {
+  t.equal( utils.hello(), "Hello TSCLI!" );
+  t.end();
+} );
+
+test( "goodbye", t => {
+  t.equal( utils.goodbye(), "See you back =)" );
+  t.end();
+} );
