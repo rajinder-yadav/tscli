cec90d3a7e4cb8b89c8e76d09fe8bb1114ea63d9
Rajinder Yadav <devguy.ca@gmail.com>
2017-03-12 01:49:47 -0500

    Dev (#35)
    * Switched to png format to get it to work with github.
    
    * NPM missing dependency fix.

--- bin/data-types/data-types.d.ts
+++ bin/data-types/data-types.d.ts
@@ -1,2 +1,2 @@
-export declare const VERSION = "0.1.2-alpha.4";
+export declare const VERSION = "0.1.2-alpha.5";
 export declare const YARN: string;
--- src/data-types/data-types.ts
+++ src/data-types/data-types.ts
@@ -1,6 +1,6 @@
 import * as sh from "shelljs";
 
 // Update for each release
-export const VERSION = "0.1.2-alpha.4";
+export const VERSION = "0.1.2-alpha.5";
 
 export const YARN = sh.which( "yarn" );
