430d8de3e2b3dab10f5b46297bbf580fea48425e
Rajinder Yadav <rajinder.yadav@hotmail.com>
2017-03-19 03:30:07 -0400

    Release build 0.1.2-alpha-16

--- bin/data-types/data-types.js
+++ bin/data-types/data-types.js
@@ -3,4 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sh = require("shelljs");
 exports.VERSION = "0.1.2-alpha.16";
 exports.YARN = sh.which("yarn");
-//# sourceMappingURL=data-types.js.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGF0YS10eXBlcy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9kYXRhLXR5cGVzL2RhdGEtdHlwZXMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSw4QkFBOEI7QUFHakIsUUFBQSxPQUFPLEdBQUcsZ0JBQWdCLENBQUM7QUFFM0IsUUFBQSxJQUFJLEdBQUcsRUFBRSxDQUFDLEtBQUssQ0FBRSxNQUFNLENBQUUsQ0FBQyJ9
--- bin/lib/utils.js
+++ bin/lib/utils.js
@@ -7,4 +7,4 @@ function goodbye() {
     return "See you later =)";
 }
 exports.goodbye = goodbye;
-//# sourceMappingURL=utils.js.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXRpbHMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvbGliL3V0aWxzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQWEsUUFBQSxLQUFLLEdBQUc7SUFDbkIsTUFBTSxDQUFDLGNBQWMsQ0FBQztBQUN4QixDQUFDLENBQUM7QUFFRjtJQUNFLE1BQU0sQ0FBQyxrQkFBa0IsQ0FBQztBQUM1QixDQUFDO0FBRkQsMEJBRUMifQ==
