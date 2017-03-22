adcc880b8d495d4c797b1be45c25604306f3edc4
Rajinder Yadav <rajinder.yadav@hotmail.com>
2017-03-22 04:31:44 -0400

    Added support to download files for e2e testing.

--- src/commands/create-new-project.ts
+++ src/commands/create-new-project.ts
@@ -1,6 +1,7 @@
 import * as sh from "shelljs";
 import * as path from "path";
 import * as fs from "fs";
+import * as utils from "../lib/utils";
 
 import { VERSION, YARN } from "../data-types/data-types";
 
@@ -39,7 +40,7 @@ export function createNewProject( cmd: any, options: any ): void {
   }
 
   sh.pushd( `${ options.project }` );
-  sh.mkdir( "./docs", "./logs" );
+  sh.mkdir( "docs", "logs" );
   sh.exec( "git init" );
   sh.exec( "git add -A" );
   sh.exec( `git commit -q -m "${ commit_message }"` );
@@ -49,6 +50,37 @@ export function createNewProject( cmd: any, options: any ): void {
     sh.exec( "yarn" );
   } else {
     sh.exec( "npm install" );
+  }
+
+  // Set up end to end testing.
+  if ( cmd.type === "js" ) {
+    sh.mkdir( "bin" );
+
+    // Download Chromedriver.
+    utils.downloadFileHttps( "https://chromedriver.storage.googleapis.com/2.28/chromedriver_linux64.zip",
+      "./bin/chromedriver_linux64.zip",
+      function( err: any ) {
+        if ( err ) {
+          console.log( err.message );
+        } else {
+          console.log( "Downloaded Chromedriver." );
+          sh.pushd( `${ options.project }/bin` );
+          sh.exec( "unzip chromedriver_linux64.zip" );
+          sh.rm( "chromedriver_linux64.zip" );
+          sh.popd();
+        }
+      } );
+
+    // Download standalone selenium-server.
+    utils.downloadFileHttp( "http://selenium-release.storage.googleapis.com/3.3/selenium-server-standalone-3.3.1.jar",
+      "./bin/selenium-server-standalone-3.3.1.jar",
+      function( err: any ) {
+        if ( err ) {
+          console.log( err.message );
+        } else {
+          console.log( "Downloaded Selenium server." );
+        }
+      } );
   }
 
   sh.popd();
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,57 @@
+import * as https from "https";
+import * as http from "http";
+import * as fs from "fs";
+
 export const hello = (): string => {
   return "Hello TSCLI!";
 };
 
 export function goodbye(): string {
   return "See you later =)";
+}
+
+/** Download a file over HTTPS
+ * @param uri {string} - URL of the file to download.
+ * @param filename {string} - Filename to be saved as.
+ * @param cb {any} - Callback function.
+ * @return {void}
+ */
+export function downloadFileHttps( uri: string, filename: string, cb: any ): void {
+  const file = fs.createWriteStream( filename );
+  /*const request =*/ https.get( uri, function( response ) {
+    response.pipe( file );
+    file.on( "finish", function() {
+      cb();
+    } )
+      .on( "error", function( err: any ) {
+        // Delete the file async, don't check the result.
+        fs.unlink( filename );
+        if ( cb ) {
+          cb( err );
+        }
+      } );
+  } );
+}
+
+/** Download a file over HTTP
+ * @param uri {string} - URL of the file to download.
+ * @param filename {string} - Filename to be saved as.
+ * @param cb {any} - Callback function.
+ * @return {void}
+ */
+export function downloadFileHttp( uri: string, filename: string, cb: any ): void {
+  const file = fs.createWriteStream( filename );
+  /*const request =*/ http.get( uri, function( response ) {
+    response.pipe( file );
+    file.on( "finish", function() {
+      cb();
+    } )
+      .on( "error", function( err: any ) {
+        // Delete the file async, don't check the result.
+        fs.unlink( filename );
+        if ( cb ) {
+          cb( err );
+        }
+      } );
+  } );
 }
