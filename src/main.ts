1f74cda93061f37bd94352abf3d07f853f930e24
Rajinder Yadav <rajinder.yadav@hotmail.com>
2017-04-29 03:58:39 -0400

    Corrected usage text.

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ cmd
   .version( `${ VERSION }` )
   .usage( "<command> <project> [options...]" )
   .arguments( "<command> <project>" )
-  .option( "-t, --type <type>", "Project types: {blank(default)|node|js}" )
+  .option( "-t, --type <type>", "Project types: {default|node|js}" )
   .action(( command: string, project: string ) => {
     Object.assign( options, { command, project } );
   } )
