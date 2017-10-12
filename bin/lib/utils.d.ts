c05efb6bc4926129521826f1d22c5db54bf7fcb6
Rajinder Yadav <rajindery@hotmail.com>
2017-10-12 04:10:55 -0400

    Build alpha.21.02

--- bin/lib/utils.d.ts
+++ bin/lib/utils.d.ts
@@ -1,16 +1,4 @@
 export declare const hello: () => string;
 export declare function goodbye(): string;
-/** Download a file over HTTPS
- * @param uri {string} - URL of the file to download.
- * @param filename {string} - Filename to be saved as.
- * @param cb {any} - Callback function.
- * @return {void}
- */
 export declare function downloadFileHttps(uri: any, filename: string, cb: any): void;
-/** Download a file over HTTP
- * @param uri {string} - URL of the file to download.
- * @param filename {string} - Filename to be saved as.
- * @param cb {any} - Callback function.
- * @return {void}
- */
 export declare function downloadFileHttp(uri: string, filename: string, cb: any): void;
